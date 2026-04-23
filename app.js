740c33a90bd434cd1a21e377f317335ffb69c526
Guilherme Durante <rxxnyt8xrj@privaterelay.appleid.com>
2026-04-23 19:50:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
        alert('Boas vindas ao jogo do número secreto');
         //parseint tira as casas decimais do Math.random
-        let numeroSecreto = parseInt(Math.random() * 10  + 1);
+        let numeroSecreto = parseInt(Math.random() * 100  + 1);
         console.log(numeroSecreto);
         let chute;
         let tentativas = 1;
 
         //enquanto o chute for diferente do numerosecreto, continua
         while(chute != numeroSecreto){
-            chute = prompt('escolha um numero entre 1 e 10');
+            chute = prompt('escolha um numero entre 1 e 100');
         // se chute for igual ao número secreto
         if (chute == numeroSecreto) {
             break;
